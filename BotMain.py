70485b688f853b3b9b2270706792e0dbc855e9a4
Dominik Jelicic <dominik246@gmail.com>
2020-04-27 21:10:00 +0200

    Removed need for local JSON file
    Added Requirements.txt
    startup.bat optimized for it
    Added SteamInfoPuller.SelectRandomGame()
    Optimized SteamInfoPuller.SteamGameSearch()

--- BotMain.py
+++ BotMain.py
@@ -1,11 +1,7 @@
-#!/usr/bin/env python3
-
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot, has_permissions
 import _asyncio
-
-
 import helpers.SteamInfoPuller
 import helpers.Flip
 from helpers.Flip import rsItem
@@ -16,10 +12,11 @@ client = commands.Bot(command_prefix='$')
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('idk wat dat')) # activity changes the "Playing" section of the status profile
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(helpers.SteamInfoPuller.SelectRandomGame()))
     print('Bot logging in as {0.user}'.format(client))
 
-############COMMANDS###############################################################
+# Start of commands
+
 @commands.cooldown(1, 3, commands.BucketType.guild)
 @client.command()
 async def ping(ctx):
@@ -30,15 +27,22 @@ async def rsitem(ctx, *item):
     item = (" ".join(item[:]))
     await ctx.channel.send(rsItem(item))
 
-
 @client.command()
 async def kill(ctx, amt):
     if ctx.author.id == 150009383592525826:
         await ctx.channel.purge(limit=int(amt)+1)
         await ctx.channel.send("Killed {}".format(amt) + " messages.")
 
+@client.command()
+async def hello(ctx):
+    await ctx.channel.send('helo dood :D')
 
-###### commands end here #######################################################
+@client.command()
+async def steam(ctx, arg):
+    await ctx.channel.send(helpers.SteamInfoPuller.SteamGameSearch(arg))
+
+
+# End of commands
 
 @client.event
 async def on_message(message):
@@ -46,29 +50,19 @@ async def on_message(message):
         return
     await client.process_commands(message)
 
-    if message.guild.id == 213868052373045248 or message.guild.id == 476472672625098752: #graveyard or bikini bottom ####################################################
-        #if message.content == 'test':
-        #    await message.channel.send(message.guild)
+    if message.guild.id == 213868052373045248 or message.guild.id == 476472672625098752: # graveyard or bikini bottom
 
         if "spopy" in message.content.lower() or "spop" in message.content.lower():
-            # await message.add_reaction(emoji='❓')
             await message.add_reaction(emoji='❓')
 
         if "anime" in message.content.lower():
             await message.add_reaction(emoji='😡')
 
-        if message.content.startswith('$hello'):
-            await message.channel.send('helo dood :D')
-
-        if message.content.startswith('$steam'):
-            game_name = message.content[7:]
-            await message.channel.send(helpers.SteamInfoPuller.steamGameSearch(game_name))
-
         if message.author.id == 578771603475791893:
             if "dark souls" in message.content.lower():
                 await message.add_reaction(emoji='😔')
 
-    if message.guild.id == 213330594749349888: #ninjacord ###################################################
+    if message.guild.id == 213330594749349888: # ninjacord
         if message.content.startswith('!spopy7'):
             await message.channel.send('https://gyazo.com/77a9b9d4cc3890b70eed030fa7d51a87')
 
